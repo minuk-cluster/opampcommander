e882dc2b1abfb68bf9568ef037c1eb35bf572eb1
Min Uk Lee <minuk.dev@gmail.com>
2024-10-19 17:11:27 +0900

    feat: Implement upsert

--- src/main/kotlin/dev/minuk/opampcommander/adapter/secondary/persistence/mongo/agent/AgentMongoAdapter.kt
+++ src/main/kotlin/dev/minuk/opampcommander/adapter/secondary/persistence/mongo/agent/AgentMongoAdapter.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.data.mongodb.core.FindAndReplaceOptions
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
@@ -60,8 +61,10 @@ class AgentMongoAdapter(
                 componentHealth = agent.componentHealth,
                 customCapabilities = agent.customCapabilities,
             )
+        val query = Query.query(Criteria.where("instanceUid").`is`(agent.instanceUid.toUuid()))
+        val options = FindAndReplaceOptions.options().upsert().returnNew()
         return reactiveMongoOperations
-            .save(agentDocument)
+            .findAndReplace(query, agentDocument, options)
             .map {
                 Agent(
                     instanceUid = Ulid.from(it.instanceUid),
--- src/test/kotlin/dev/minuk/opampcommander/adapter/secondary/persistence/mongo/agent/AgentMongoAdapterTest.kt
+++ src/test/kotlin/dev/minuk/opampcommander/adapter/secondary/persistence/mongo/agent/AgentMongoAdapterTest.kt
@@ -2,6 +2,7 @@ package dev.minuk.opampcommander.adapter.secondary.persistence.mongo.agent
 
 import com.github.f4b6a3.ulid.Ulid
 import dev.minuk.opampcommander.adapter.secondary.persistence.mongo.agent.document.AgentDocument
+import dev.minuk.opampcommander.domain.models.agent.Agent
 import dev.minuk.opampcommander.domain.models.agent.AgentDescription
 import dev.minuk.opampcommander.domain.models.agent.CommunicationStatus
 import dev.minuk.opampcommander.domain.models.agent.ComponentHealth
@@ -16,7 +17,10 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.test.context.junit4.SpringRunner
+import reactor.test.StepVerifier
 import kotlin.test.assertNull
 
 @RunWith(SpringRunner::class)
@@ -24,7 +28,7 @@ import kotlin.test.assertNull
 class AgentMongoAdapterTest(
     @Autowired
     private val reactiveMongoOperations: ReactiveMongoOperations,
-){
+) {
     val agentMongoAdapter = AgentMongoAdapter(reactiveMongoOperations)
 
     @BeforeEach
@@ -36,7 +40,7 @@ class AgentMongoAdapterTest(
     }
 
     @Test
-    fun `getAgentByInstanceUid should return agent when agent exists`() {
+    fun `getAgentByInstanceUid() should return agent when agent exists`() {
         // given
         val instanceUid = Ulid.fast()
         reactiveMongoOperations.insert(emptyAgentDocument().copy(instanceUid = instanceUid.toUuid())).block()
@@ -47,7 +51,7 @@ class AgentMongoAdapterTest(
     }
 
     @Test
-    fun `getAgentByInstanceUid should return null when agent not exists`() {
+    fun `getAgentByInstanceUid() should return null when agent not exists`() {
         // given
         val instanceUid = Ulid.fast()
         // when
@@ -56,16 +60,53 @@ class AgentMongoAdapterTest(
         assertNull(agent)
     }
 
-    private fun emptyAgentDocument() = AgentDocument(
-        instanceUid = Ulid.fast().toUuid(),
-        agentDescription = AgentDescription(
-            identifyingAttributes = emptyMap(),
-            nonIdentifyingAttributes = emptyMap(),
-        ),
-        effectiveConfig = EffectiveConfig.empty(),
-        communicationStatus = CommunicationStatus.empty(),
-        packageStatuses = PackageStatuses.empty(),
-        componentHealth = ComponentHealth.empty(),
-        customCapabilities = CustomCapabilities.empty(),
-    )
+    @Test
+    fun `saveAgent() should save agent to database`() {
+        // given
+        val instanceUid = Ulid.fast()
+        val agent = Agent(instanceUid = instanceUid)
+
+        // when
+        runBlocking { agentMongoAdapter.saveAgent(agent) }
+
+        // then - 1
+        StepVerifier
+            .create(
+                reactiveMongoOperations.findOne(
+                    Query.query(
+                        Criteria.where("instanceUid").`is`(instanceUid.toUuid()),
+                    ),
+                    AgentDocument::class.java,
+                ),
+            ).assertNext {
+                assertNotNull(it)
+            }.verifyComplete()
+
+        // then - 2
+        reactiveMongoOperations
+            .findOne(
+                Query.query(
+                    Criteria.where("instanceUid").`is`(instanceUid.toUuid()),
+                ),
+                AgentDocument::class.java,
+            ).block()
+            ?.let {
+                assertNotNull(it)
+            }
+    }
+
+    private fun emptyAgentDocument() =
+        AgentDocument(
+            instanceUid = Ulid.fast().toUuid(),
+            agentDescription =
+                AgentDescription(
+                    identifyingAttributes = emptyMap(),
+                    nonIdentifyingAttributes = emptyMap(),
+                ),
+            effectiveConfig = EffectiveConfig.empty(),
+            communicationStatus = CommunicationStatus.empty(),
+            packageStatuses = PackageStatuses.empty(),
+            componentHealth = ComponentHealth.empty(),
+            customCapabilities = CustomCapabilities.empty(),
+        )
 }
